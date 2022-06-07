c843898d50c574f03da3822dd20f56ccadddf1cc
behfarkhosravi <behfar.khosravi@gmail.com>
2022-06-07 13:14:38 +0430

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
     colorMode: {
       defaultMode: "dark",
       disableSwitch: false,
-      respectPrefersColorScheme: true,
+      respectPrefersColorScheme: false,
     },
     announcementBar: {
       id: 'warn',
