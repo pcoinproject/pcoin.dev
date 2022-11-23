3ef5c0c8245baa4ce2a1a4c5db074ed61ae65426
behfarkhosravi <behfar.khosravi@gmail.com>
2022-11-23 14:09:59 +0330

    Update FAQ

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -12,20 +12,24 @@ interface Question {
 
 const data: Question[] = [
   {
-    title: "What is Pcoin?",
-    description: <>Todo</>,
+    title: "How Fast Is A Pcoin Transaction?",
+    description:
+      "Pcoin transactions are incredibly fast. Watch payments remit in < 1 second, with full ability to spend/use those funds after 6 confirmations (~ 6 minutes).",
   },
   {
-    title: "Todo",
-    description: <>Todo</>,
+    title: "How Low Are The Network Fees For Pcoin?",
+    description:
+      "Pcoin has consistently had some of the lowest transaction fees in the industry (typical sends “cost” the sender < 0.003 Pcoin (< $0.01) per transaction. Remember, these fees are burnt from the total coin supply, thus reducing inflation assisting in stabilizing the coin value.",
   },
   {
-    title: "Todo",
-    description: <>Todo</>,
+    title: "Why do you even have a “Governance”?",
+    description:
+      "We feel that if you are your own bank, you should have a choice in what direction the bank goes. Voting in our DAO/Governance can be making important changes or trivial ones just like in real life.",
   },
   {
-    title: "Todo",
-    description: <>Todo</>,
+    title: "What is Staking?",
+    description:
+      "Staking is the process of locking your coins for any time period set by you in order to help the network achieve consensus of proof of stake by validating every transaction that occurs on the network to be written permanently to the blockchain. In return stakers that mint the next block are rewarded a block reward.",
   },
 ]
 
